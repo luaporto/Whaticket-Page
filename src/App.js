e62f4d84b46b0e29470adcd886fe5da08c209498
Molekulah <bruno@economicros.com.br>
2021-10-01 17:15:01 -0300

    Created TopBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 // import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Button, Stack, CssBaseline } from "@mui/material";
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import TopBar from "./Components/TopBar";
+
+const theme = createTheme({
+  status: {
+    danger: "#e53e3e",
+  },
+  palette: {
+    primary: {
+      main: "#00ACC1",
+      contrastText: "#FFFFFF",
+      darker: "#053e85",
+    },
+    secondary: {
+      main: "#121212",
+      darker: "#053e85",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>Whaticket</p>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <TopBar />
+      </ThemeProvider>
+    </>
   );
 }
 
