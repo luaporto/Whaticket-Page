b43a03fa5fc82cf4f67534bfdd2080922f19d899
Luahp <luahporto@gmail.com>
2021-10-05 13:22:22 -0300

    custom icon

--- src/App.js
+++ src/App.js
@@ -5,38 +5,38 @@ import {
   Stack,
   CssBaseline,
   Typography,
-} from "@mui/material";
-import "./App.css";
-import { createTheme, ThemeProvider } from "@mui/material";
-import TopBar from "./Components/TopBar";
-import Page from "./Page";
-import darkScrollbar from "@mui/material/darkScrollbar";
+} from '@mui/material';
+import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+import TopBar from './Components/TopBar';
+import Page from './Page';
+import darkScrollbar from '@mui/material/darkScrollbar';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#00ACC1",
-      contrastText: "#FFFFFF",
-      darker: "#053e85",
+      main: '#00ACC1',
+      contrastText: '#FFFFFF',
+      darker: '#053e85',
     },
     secondary: {
-      main: "#121212",
-      darker: "#053e85",
+      main: '#121212',
+      darker: '#053e85',
     },
   },
 });
 
 const scroll = createTheme({
-  "@global": {
-    "*::-webkit-scrollbar": {
-      width: "0.4em",
+  '@global': {
+    '*::-webkit-scrollbar': {
+      width: '0.4em',
     },
-    "*::-webkit-scrollbar-track": {
-      "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,0.00)",
+    '*::-webkit-scrollbar-track': {
+      '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
     },
-    "*::-webkit-scrollbar-thumb": {
-      backgroundColor: "rgba(0,0,0,.1)",
-      outline: "1px solid slategrey",
+    '*::-webkit-scrollbar-thumb': {
+      backgroundColor: 'rgba(0,0,0,.1)',
+      outline: '1px solid slategrey',
     },
   },
 });
@@ -47,28 +47,28 @@ function App() {
       <CssBaseline />
 
       <ThemeProvider theme={theme}>
-        <Container sx={{ backgroundColor: "#ffffff" }}>
+        <Container sx={{ backgroundColor: '#ffffff' }}>
           <TopBar />
           <Page />
           <Box style={{ height: 10 }}></Box>
           <Box>
-            {" "}
+            {' '}
             <Typography
               sx={{
-                width: "448px",
-                height: "49px",
+                width: '448px',
+                height: '49px',
 
-                fontFamily: "Nunito",
-                fontStyle: "normal",
+                fontFamily: 'Nunito',
+                fontStyle: 'normal',
                 fontWeight: 800,
-                fontSize: "36px",
-                lineHeight: "49px",
-                letterSpacing: "0.025em",
-                color: "#302F2F",
+                fontSize: '36px',
+                lineHeight: '49px',
+                letterSpacing: '0.025em',
+                color: '#302F2F',
               }}
             >
               q terror
-            </Typography>{" "}
+            </Typography>{' '}
           </Box>
           <Box style={{ height: 10000 }}></Box>
         </Container>
--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,6 +1,6 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
-import "../App.css";
-import icon from "../assets/icon.png";
+import { Box, Button, Stack, Typography } from '@mui/material';
+import '../App.css';
+import icon from '../assets/icon.png';
 
 const TopBar = () => {
   return (
@@ -8,22 +8,22 @@ const TopBar = () => {
       <Box
         sx={{
           zIndex: 100,
-          backgroundColor: "#ffffff",
-          position: "sticky",
-          top: "0px",
-          height: "25px",
+          backgroundColor: '#ffffff',
+          position: 'sticky',
+          top: '0px',
+          height: '25px',
         }}
       ></Box>
       <Box
         sx={{
           zIndex: 100,
-          fontStyle: "normal",
-          fontSize: "20px",
-          letterSpacing: "0.025em",
-          position: "sticky",
-          top: "10px",
-          backgroundColor: "#ffffff",
-          height: "75px",
+          fontStyle: 'normal',
+          fontSize: '20px',
+          letterSpacing: '0.025em',
+          position: 'sticky',
+          top: '10px',
+          backgroundColor: '#ffffff',
+          height: '75px',
         }}
       >
         <Stack
@@ -32,17 +32,31 @@ const TopBar = () => {
           alignItems="center"
         >
           <Stack direction="row">
+            <img
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                paddingRight: '2px',
+                width: 100,
+                height: 100,
+                paddingRight: 11,
+              }}
+              src={icon}
+              alt=""
+            />
+
             <Typography
-              sx={{ fontSize: "36px", fontFamily: "Mada", fontWeight: "400" }}
+              sx={{ fontSize: '36px', fontFamily: 'Mada', fontWeight: '400' }}
             >
               Wha
             </Typography>
             <Typography
-              sx={{ fontSize: "36px", fontFamily: "Mada", fontWeight: "700" }}
+              sx={{ fontSize: '36px', fontFamily: 'Mada', fontWeight: '700' }}
             >
               ticket
             </Typography>
           </Stack>
+
           <Stack direction="row" spacing={2}>
             <Button color="secondary">Entrar</Button>
             <Button variant="contained" color="primary">
