1ae10365feee39f83152182d4127744ef53d9b56
Molekulah <bruno@economicros.com.br>
2021-10-07 14:35:38 -0300

    Created pricing div

--- src/Page.js
+++ src/Page.js
@@ -121,11 +121,6 @@ const Page = ({
           ></Box>
         </Grid>
       </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <img src={multipleUsers} alt="" />
-        </Grid>
-      </Grid>
       <Grid container spacing={10} sx={{ paddingBottom: "200px" }}>
         <Grid item xs={4}>
           <Box
@@ -150,7 +145,6 @@ const Page = ({
             </TextTypography>
           </Stack>
         </Grid>
-
         <Grid item xs={4}>
           <Box
             sx={{
@@ -276,6 +270,7 @@ const Page = ({
           </TextTypography>
         </Grid>
         <Grid
+          sx={{}}
           item
           xs={6}
           sx={{
@@ -291,13 +286,26 @@ const Page = ({
       <Box sx={{ paddingBottom: "100px" }}>
         <img src={whaticketPrint} alt=""></img>
       </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          width: "1450px",
+          height: "500px",
+          zIndex: "1",
+          marginTop: "150px",
 
+          background: "#FFED9C",
+          borderRadius: "720px 0px 0px 0px",
+          transform: "matrix(-1, 0, 0, 1, 0, 0)",
+        }}
+      />
       <Grid container spacing={2}>
         <Box
           sx={{
             letterSpacing: "0.025em",
             textAlign: "center",
             fontStyle: "normal",
+            zIndex: "20",
           }}
         >
           <Typography
@@ -324,7 +332,7 @@ const Page = ({
             seu atendimento.
           </TextTypography>
         </Box>
-        <Grid item xs={4}>
+        <Grid item xs={4} sx={{ zIndex: "20" }}>
           <Card sx={{ maxWidth: 345, borderRadius: "20px 20px 20px 20px" }}>
             <Box
               sx={{
@@ -410,7 +418,7 @@ const Page = ({
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} sx={{ zIndex: "20" }}>
           <Card
             sx={{
               maxWidth: 345,
@@ -436,7 +444,7 @@ const Page = ({
                   }}
                 >
                   {" "}
-                  Código aberto{" "}
+                  Pro{" "}
                 </PriceTitleTypography>{" "}
                 <TextTypography
                   sx={{
@@ -515,7 +523,7 @@ const Page = ({
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} sx={{ zIndex: "20" }}>
           <Card sx={{ maxWidth: 345, borderRadius: "20px 20px 20px 20px" }}>
             <Box
               sx={{
@@ -534,7 +542,7 @@ const Page = ({
                 }}
               >
                 {" "}
-                Código aberto{" "}
+                Enterprise{" "}
               </PriceTitleTypography>
             </Box>
             <CardContent sx={{ textAlign: "center" }}>
@@ -546,7 +554,7 @@ const Page = ({
                 }}
               >
                 <PriceTypography sx={{ color: "#25385C" }}>
-                  R$ 0
+                  R$ 699,00
                 </PriceTypography>
                 <PriceTimeTypography sx={{ color: "#25385C" }}>
                   /mês
